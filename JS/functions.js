30b354f2c73159aa8e63da2ed6375ea3295af26b
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-03 15:19:49 +0200

    scopes

--- JS/functions.js
+++ JS/functions.js
@@ -42,3 +42,59 @@ true
 //#############################################################
 //###############################################################
 
+// user defined methods  
+
+function addTwoNums(x, y) { 
+  sum = x + y;
+  return sum;
+}
+undefined
+result = addTwoNums(2, 7)
+// => 9 
+
+// default values 
+
+function addTwoNums(x = 0, y = 0) { 
+  x =(x==undefined) ? 0 : x; 
+  y =(y==undefined) ? 0 : y;
+  sum = x + y;
+  return sum;
+}
+
+result = addTwoNums()
+console.log(result);
+
+
+
+//#############################################################
+//###############################################################
+
+// scope and shadwoing 
+
+// var preserve variables from breaking scopes 
+// shadwoing happens when we set two variables with same name one local and other global  
+// shadwoing var without var will override the global scope
+// global variables defined outside functions  
+// local scope within the fun defenition 
+// onc we create varibale without var in local scope and call it in global will be visible(global) at first call to the fun only  
+// 
+
+console.log("<br>")
+var z = 40; 
+var x = 12; 
+
+function test(y) { 
+  var z = 9; // shadwoing overwrite z 
+  console.log(z) // print 9 
+  console.log(x) // print global x 
+  x = 15;  // overwrite x
+  console.log(x) // print local x
+  var sum = y + z 
+  return sum;
+}
+console.log(x) // => 12 global 
+console.log(z) // => 40 global
+result = test(4); // function call 
+console.log(z) // => z = 40 global  
+console.log(x)  // => overwrite x with first func call 
+console.log(result);
