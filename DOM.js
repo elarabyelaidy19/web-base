11b912e3f86adc9534ec92a8c670d931cea2faf8
elaraby elaidy <elarabyelaidy@gmail.com>
2021-10-11 19:27:50 +0200

    cookie

--- DOM.js
+++ DOM.js
@@ -39,3 +39,80 @@ ele[0].style.color="red"
 
 
 // NODES CLASS ATTRIBUTES
+// add OR remove classes 
+
+document.getElementById("div1").className // return node class name 
+document.getElementById("div1").classList.add("c2") // add class to node 
+document.getElementById("div1").classList.remove("c2") // remove 
+getComputedStyle(getElementById("div1")).color // return style of the node  
+
+
+// CREATE NEW DOM NODE
+
+// Types of Nodes 
+// 1- root node => html 
+// 2- element node => any tag 
+// 3- Text node => text of any tag 
+// 4- Attribute node => attribute of tag
+// 5- Cooment node => comment 
+// 6- Document node => the document itself is node  
+
+document.createElement() // create element node 
+document.createTextNode()
+// METHOD 
+insertBefore()
+document.insertBefore(a,b) // a = inserted b = before  
+a.appendChild(b) // a == parent  b = appended child 
+
+
+
+/// DOM COLLECTION 
+// elements collection 
+// access element of collection 
+document.images[0] // access elemnt 1 of images collection 
+
+
+//#############################################################
+//###############################################################
+
+// Cookies 
+
+// create cookie
+function saveInfo() { 
+  var date = new Date();
+  date.setDate(date.getMonth() + 3);
+  var val = document.getElementById("usrnm").value;
+  var val2 = document.getElementById("mail").vlaue;
+
+  // persitent cookie
+  document.cookie = "usernameCookie = " +val+";expires = "+date; 
+  document.cookie = "userEmailCookie = " +val2+";expires = "+date;
+}
+
+
+// READ COOKIE
+document.cookie
+
+//#############################################################
+//###############################################################
+
+ // DISPLAY KEY VALUE PAIRS COOKIE 
+ var cookie = document.cookie();
+cookie = "username=ali;epires=122332"
+ cookie.indexOf("username") 
+ // => 0 
+ cookie.indexOf(":",0); 
+ //=> 12
+cookie.subString(0,18)
+
+//#############################################################
+//###############################################################
+
+
+//#############################################################
+//###############################################################
+
+
+//#############################################################
+//###############################################################
+
